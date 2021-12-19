67648c0db535f2ae7631b2954aa75cf762de01a6
Yuhsuan <nyto9999@gmail.com>
2021-12-19 23:52:22 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\AirConController;
 Route::get('/', function () {
     return view('welcome');
 });
-//tonyadsds
+//tonyadsdssssss
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
