77c3e4a1496c907efa4acafba4c12a34fba33978
Yuhsuan <nyto9999@gmail.com>
2021-12-20 00:07:36 +0800

    321

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//I add a route hereddddsfsss
+//I add a route hereddddsfssssdf
 Auth::routes();
 
 
