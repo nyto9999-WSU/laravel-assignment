536480b1ef811ccbe0752adb6a737618d7702689
ninjachen <twtc2222@gmail.com>
2021-12-19 17:40:04 +0800

    aaa

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $guarded = [];
-
+    //modify
     /**
      * The attributes that should be hidden for serialization.
      *
